b225bc3556f4ef314ebc4697d2800f375bac9761
Komal Meena <capkomal2203@gmail.com>
2025-01-27 13:04:42 +0530

    cookies

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ dotenv.config({
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000)
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port:${process.env.PORT}`);
+    });
   })
   .catch((error) => {
     console.log("MONGODB connection failed: ", error);
