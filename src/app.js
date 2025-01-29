c14288cbb8b1e5ccfd416ed96573b4b4c2773f4d
Komal Meena <capkomal2203@gmail.com>
2025-01-29 22:20:49 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -12,10 +12,14 @@ app.use(
 );
 
 app.use(express.json({ limit: "16kb" }));
-
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-export { app };
-
 app.use(express.static("public"));
-
 app.use(cookieParser());
+
+//routes
+import userRouter from "./routes/user.routes.js";
+
+//routes decalaration
+app.use("/api/v1/users", userRouter);
+
+export { app };
